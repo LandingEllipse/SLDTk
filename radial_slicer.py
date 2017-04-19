Refactor radial slicer to use cv2 rotation about disk centre and pure numpy. Fixes #3.
The slicer now handles offset disks and returns an ndarray stack of the extracted slices.
Future optimisation could be made by extracting slices in both directions from the centre, but care should be taken to ensure slices are stored in sequence for stack image output purposes.

radial_slicer.py
@@ -1,36 +1,53 @@
-from pylab import imshow, figure, zeros, plot
-from scipy.misc import imread
-from scipy.ndimage.interpolation import rotate
-from numpy import savetxt
 import numpy as np
-import matplotlib.pyplot as plt
-
-a = imread("testing/20170315_130000_4096_HMIIC_-watermark.jpg", flatten=True)
-#imshow(a)
-shape = a.shape
-print(shape[1])
-
-slices = 20
-
-stack = zeros((int(shape[1]/2), slices))  # create an array to save the slices
-total = stack[:, 0]  # create an array to save the average slice
-
-for i in range(slices):  # take ten slices
-    print(i)
-    stack[:, i] = rotate(input=a, angle=(i * (360/slices)), reshape=False)[int(shape[1]/2), int(shape[1]/2):]  # TODO: just get img centre as arg to class...
-    plt.plot(stack[:, i])
-    total += stack[:, i]
-    # print(total)
-
-total = total / (slices + 1)
-plot(total)
-
-plt.savefig("out/intensity_graph_orange")
-plt.show()
-savetxt("out/jefferson-v2.dat", total)  # save the data to a file
-
-########################################################################################################################
-
-import pandas as pd
-import matplotlib.pyplot as plt
 from scipy.ndimage.interpolation import rotate
+
+
+def radial_slice(img, center, radius, slices=20):
+    # radius = int(radius * 1.05)  # give us some headroom to ensure the limb isn't clipped
+    radius = radius + 10  # TODO: determine radius to use based on min of radius*1.05 and center->img_edge (up/down/left/right)
+    stack = np.zeros((slices, radius), img.dtype)
+
+    # TODO: optimize to look both left and right from center for each rotation - place cnt->r in stack[i] and cnt->-r in stack[-i]?
+    for i in range(slices):
+        matrix = cv2.getRotationMatrix2D(center=(center[0], center[1]), angle=(i * (360 / slices)), scale=1)
+        rotated = cv2.warpAffine(img, matrix, (img.shape[1], img.shape[0]))
+        # rotated = rotate(input=img, angle=(i * (360 / slices)), reshape=False)
+        stack[i, :] = rotated[center[0], center[1]:center[1] + radius]  # FIXME: verify x/y -> h/w
+        # cv2.imwrite("out/radial_slicer/rotation/rotated{}.png".format(i), rotated)  # can be used to create animations
+        # plt.plot(stack[i, :])
+
+    return stack
+
+
+if __name__ == "__main__":
+    import cv2
+    import matplotlib.pyplot as plt
+    import disk_analyzer
+
+    path1 = "tests/images/20170315_130000_4096_HMIIC_-watermark_small.jpg"
+    path2 = "tests/images/20170315_130000_4096_HMIIC_-watermark_small_offset.jpg"
+    path3 = "tests/images/20170315_aberystwyth_combined.jpg"
+    path4 = "tests/images/20170315_aberystwyth_combined_square.jpg"
+
+    image = cv2.imread(path1, cv2.IMREAD_GRAYSCALE)
+
+    center, radius = disk_analyzer.analyze_disk(img=image.copy(), threshold=20)
+
+    print("min enclosing circle x,y: {},{}".format(center[0], center[1]))
+    print("min enclosing circle radius: {}".format(radius))
+
+    cv2.imwrite("out/radial_slicer/input_image.png", image)
+
+    from timeit import default_timer as timer
+    start = timer()
+    image_sliced = radial_slice(img=image, center=center, radius=radius, slices=20)
+    end = timer()
+    print(end - start)
+
+    median = np.median(image_sliced, axis=0)
+    plt.plot(median)
+    mean = np.mean(image_sliced, axis=0)
+    plt.plot(mean)
+    plt.show()
+
+    cv2.imwrite("out/radial_slicer/slices.png", image_sliced)