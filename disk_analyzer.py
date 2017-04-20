136270829a87a48b1a1da910c52a8c422d41563f
Ariel Ladegaard <legendin@gmail.com>
2017-04-20 21:35:38 +0100

    Update disk_analyzer to resist detecting watermarks. Fixes #10.

--- disk_analyzer.py
+++ disk_analyzer.py
@@ -3,41 +3,46 @@ import cv2
 
 
 def analyze_disk(img, threshold=10):
-    """Finds the center and radius of a single disk present in the supplied image.
+    """Finds the center and radius of a single solar disk present in the supplied image.
     
-    Uses cv2.Canny, cv2.findContours and cv2.minEnclosingCircle to determine the centre and 
+    Uses cv2.inRange, cv2.findContours and cv2.minEnclosingCircle to determine the centre and 
     radius of the solar disk present in the supplied image.
     
     Args:
-        img (numpy.ndarray): greyscale image containing a single disk in high contrast to the background.
+        img (numpy.ndarray): greyscale image containing a full, single solar disk against a background that is below `threshold`.
         threshold (int): threshold of min pixel value to include in the solar disk
     
     Returns:
-        tuple: center coordinates (int) 
+        tuple: center coordinates in x,y form (int) 
         int: radius
     """
     if img is None:
-        raise TypeError("Supplied image is None - check that the path of the loaded image is correct.")
+        raise TypeError("img argument is None - check that the path of the loaded image is correct.")
 
     if len(img.shape) > 2:
-        raise TypeError("Expected grayscale image.")  # TODO: just do grayscale transformation internally?
+        raise TypeError("Expected single channel (grayscale) image.")
 
     blur = cv2.GaussianBlur(img, (5, 5), 0)
     mask = cv2.inRange(blur, threshold, 255)
     img_mod, contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-
-    # TODO: remove debug output
-    print("Number of contours found: {}".format(len(contours)))
-    cv2.imwrite("out/disk_analyzer/mask.png", mask)
-    cv2.drawContours(img, contours, -1, (0, 0, 255), -1)
+    # find and use the biggest contour
+    r = 0
     for cnt in contours:
-        (x, y), r = cv2.minEnclosingCircle(cnt)
-        cv2.circle(img, (round(x), round(y)), round(r), (0, 0, 255), 2)
-    cv2.imwrite("out/disk_analyzer/circled_contours.png", img)
+        (c_x, c_y), c_r = cv2.minEnclosingCircle(cnt)
+        # cv2.circle(img, (round(c_x), round(c_y)), round(c_r), (255, 255, 255), 2)
+        if c_r > r:
+            x = c_x
+            y = c_y
+            r = c_r
 
+    # print("Number of contours found: {}".format(len(contours)))
+    # cv2.imwrite("out/disk_analyzer/mask.png", mask)
+    # cv2.imwrite("out/disk_analyzer/circled_contours.png", img)
 
-    (x, y), r = cv2.minEnclosingCircle(contours[0])  # use the first/primary contour detected
+    if x is None:
+        raise RuntimeError("No disks detected in the image.")
+
     return (round(x), round(y)), round(r)
 
 
@@ -45,8 +50,10 @@ if __name__ == "__main__":
     path = "tests/images/20170315_130000_4096_HMIIC_-watermark.jpg"
     path2 = "tests/images/20170315_aberystwyth_combined.jpg"
     path5 = "tests/images/LimbDark.png"
+    path7 = "tests/images/20170420_4096_HMIIC.jpg"
+    path8 = "tests/images/20170420_4096_HMIIC_-watermark.jpg"
 
-    image = cv2.imread(path5)
+    image = cv2.imread(path7)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     center, radius = analyze_disk(img=gray, threshold=20)
