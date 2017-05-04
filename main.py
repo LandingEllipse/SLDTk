ccab25a1a78937825f59ab3229b7df5f3c0177ca
Ariel Ladegaard <legendin@gmail.com>
2017-05-04 17:08:28 +0100

    Change input args validation to accomodate new slicing approach. Change plot layout/formatting.

--- main.py
+++ main.py
@@ -42,13 +42,13 @@ def generate_output_paths(args):
     return paths
 
 
-def pos_even_int(arg):
+def pos_mul_4_int(arg):
     try:
         val = int(arg)
     except ValueError:
         raise argparse.ArgumentTypeError("{} could not be interpreted as an integer.".format(arg))
     else:
-        if val < 0 or val % 2 is not 0:  # TODO: might not need to enforce % when/if the implementation is changed to polar
+        if val < 0 or val % 4 != 0:
             raise argparse.ArgumentTypeError("{} is not a positive, even integer.".format(val))
     return val
 
@@ -69,7 +69,7 @@ def parse_input():
     ap.add_argument("-i", "--image", required=True, help="path to a jpg or png solar image file")
     ap.add_argument("-o", "--operation", choices=config["operations"], default=config["operations"][0], help="the operation that should be performed on the image")
     ap.add_argument("-c", "--cmode", choices=config["cmode"], default=config["cmode"][0], help="the correction mode to use")
-    ap.add_argument("-s", "--slices", type=pos_even_int, default=config["slices"], help="number of slices to average to create the intensity profile")
+    ap.add_argument("-s", "--slices", type=pos_mul_4_int, default=config["slices"], help="number of slices to average to create the intensity profile")
     ap.add_argument("-t", "--threshold", type=uint8, default=config["threshold"], help="brightness threshold for the solar disk (uint8)")
     ap.add_argument("-b", "--bias", type=uint8, default=config["bias"], help="brightness bias for the correction (uint8)")
     ap.add_argument("-d", "--debug", type=bool, default=config['debug'], help="if enabled, provides intermediary output to the 'debug' directory")
@@ -92,16 +92,29 @@ def main():
     if len(image.shape) > 2:  # color image
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     else:
-        gray = image
+        gray = image.copy()
 
     # Detect the solar disk
     disk_attr = disk_detection.detect_disk(gray, args['threshold'])
+    if args['debug']:
+        print("Min enclosing circle x: {}, y: {}, r: {}".format(disk_attr[0], disk_attr[1], disk_attr[2]))
+        cv2.circle(image, (disk_attr[0], disk_attr[1]), disk_attr[2], (0, 255, 0), 6)
+        cv2.rectangle(img=image, pt1=(disk_attr[0] - 10, disk_attr[1] - 10), pt2=(disk_attr[0] + 10, disk_attr[1] + 10), color=(0, 255, 0), thickness=-1)
+        cv2.imwrite(out_paths['debug_mec'], image)
 
     # Create a slice stack
     stack = profile_generation.create_slice_stack(gray, disk_attr, args['slices'])
     stack_clean = profile_generation.clean_stack(stack)  # TODO: add 'gradient' arg for sensitivity?
     # Average the stack to create an intensity profile
     intensity_profile = profile_generation.compress_stack(stack)
+    if args['debug']:
+        # Slicing
+        print("Slices: {}".format(len(stack)))
+        print("Slices dropped: {}".format(len(stack) - len(stack_clean)))
+        cv2.imwrite(out_paths['debug_stack'], stack)
+        print("Slice stack saved to {}".format(out_paths['debug_stack']))
+        cv2.imwrite(out_paths['debug_stack_clean'], stack_clean)
+        print("Clean slice stack saved to {}".format(out_paths['debug_stack_clean']))
 
     # Attempt to model the data as a polynomial
     if args['operation'] in ('all', 'model') or args['cmode'] == 'model':
@@ -110,10 +123,11 @@ def main():
 
     # Plot intensity profile together with computed model
     if args['operation'] in ('all', 'model'):
-        plotter = plotting.Plotter(out_paths['plot'])
+        img_name, _ = os.path.splitext(os.path.basename(args['image']))
+        plotter = plotting.Plotter(img_name, out_paths['plot'])
         plotter.plot_intensity_profile(intensity_profile)
-        plotter.plot_model(model)
         plotter.plot_expected_model(model, (0.3, 0.93, -0.23))  # TODO: tmp coefs for 5500Å
+        plotter.plot_model(model)
         # plotter.show()
         plotter.save()
         print("Intensity profile plot saved to {}".format(out_paths['plot']))
@@ -126,22 +140,6 @@ def main():
             corrected = correction.correct_disk(gray, disk_attr, args['bias'], model=model)
         cv2.imwrite(out_paths['corrected'], corrected)
         print("Corrected image saved to {}".format(out_paths['corrected']))
-
-    # Provide additional information and images if debug is enabled
-    if args['debug']:
-        # Disk detection
-        print("Min enclosing circle x: {}, y: {}, r: {}".format(disk_attr[0], disk_attr[1], disk_attr[2]))
-        cv2.circle(image, (disk_attr[0], disk_attr[1]), disk_attr[2], (0, 255, 0), 6)
-        cv2.rectangle(img=image, pt1=(disk_attr[0] - 10, disk_attr[1] - 10), pt2=(disk_attr[0] + 10, disk_attr[1] + 10), color=(0, 255, 0), thickness=-1)
-        cv2.imwrite(out_paths['debug_mec'], image)
-
-        # Slicing
-        print("Slices: {}".format(len(stack)))
-        print("Slices dropped: {}".format(len(stack) - len(stack_clean)))
-        cv2.imwrite(out_paths['debug_stack'], stack)
-        print("Slice stack saved to {}".format(out_paths['debug_stack']))
-        cv2.imwrite(out_paths['debug_stack_clean'], stack_clean)
-        print("Clean slice stack saved to {}".format(out_paths['debug_stack_clean']))
 
 
 if __name__ == "__main__":
--- plotting.py
+++ plotting.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 class Plotter(object):
-    def __init__(self, out_path):
+    def __init__(self, img_name, out_path):
         self.out_path = out_path
         self.fig, self.ax = plt.subplots()
 
@@ -14,28 +14,27 @@ class Plotter(object):
         self.ax.set_xlabel("Relative Distance From Centre")
         self.ax.set_ylabel("Relative Intensity")
         self.fig.subplots_adjust(top=0.92)
-        self.fig.suptitle("Intensity Profile", size=18)  # TODO: find a better title
+        self.extra_artists = list()  # Added to extra artists during render to prevent cutoff
+        self.extra_artists.append(self.fig.suptitle("{} - Intensity Profile".format(img_name), size=14))
 
     def plot_intensity_profile(self, profile):
         y = profile / profile[0]
         x = np.linspace(0., 1., num=len(profile))
-        self.ax.scatter(x, y, s=4, c='b')
+        self.ax.scatter(x, y, s=3, c='b', label=r"$\mathtt{Intensity\ \ profile}$", zorder=1)
         # TODO: add legend
 
     def plot_model(self, model):
-        x = np.linspace(0., 1., num=200)
+        x = np.linspace(0., 1., num=400)
         y = model.evaluate(x, relative=True)
-        self.ax.plot(x, y, linewidth=2, c='r')
-        # TODO: add coefficients
-        # TODO: add legend
+        label = r"$\mathtt{{Fitted: \ \ \ \ \ \ a_0={:.2f}, a_1={:.2f}, a_2={:.2f}}}$".format(*model.coefs)
+        self.ax.plot(x, y, linewidth=2, c='r', label=label, zorder=3)
 
     # TODO: change implementation to something sensible
     def plot_expected_model(self, model, coefs):
-        x = np.linspace(0., 1., num=200)
+        x = np.linspace(0., 1., num=400)
         y = model.evaluate(x, relative=True, coefs=coefs)
-        self.ax.plot(x, y, linewidth=2, c='g')
-        # TODO: add coefficients
-        # TODO: add legend
+        label = r"$\mathtt{{Expected: a_0={:.2f}, a_1={:.2f}, a_2={:.2f}}}$".format(*coefs)
+        self.ax.plot(x, y, linewidth=2, c='g', label=label, linestyle=":", zorder=2)
 
     def show(self):
         plt.show()  # FIXME: self.fig.show() shows empty plot
@@ -43,5 +42,6 @@ class Plotter(object):
     def save(self, out_path=None, dpi=160):
         if not out_path:
             out_path = self.out_path
-        self.fig.savefig(out_path, dpi=dpi)
+        self.extra_artists.append(self.ax.legend(loc='lower left', bbox_to_anchor=(0, 0), fontsize=11))
+        self.fig.savefig(out_path, dpi=dpi, bbox_extra_artists=self.extra_artists, bbox_inches='tight')
 
